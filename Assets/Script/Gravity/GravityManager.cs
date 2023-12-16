60a942e5237367e312db4ff581f8398b1928e153
Rin81401101 <57429317+Rin81401101@users.noreply.github.com>
2023-12-17 02:30:10 +0900

    下記ファイルの関数名と変数名の見直し修正 PlayerManager.cs PlayerAnimationManager.cs GravityManager.cs

--- Assets/Script/Gravity/GravityManager.cs
+++ Assets/Script/Gravity/GravityManager.cs
@@ -6,17 +6,17 @@ public class GravityManager : MonoBehaviour
 {
     // Start is called before the first frame update
     [Header("èdóÕâ¡ë¨ìx"), SerializeField]
-    private float gravity;
+    private float m_gravity;
 
-    public static GravityManager instance;
+    public static GravityManager m_instance;
 
     private void Awake()
     {
-        instance = this;
+        m_instance = this;
     }
 
     public void gravityUpdate(Rigidbody rigidbody)
     {
-        rigidbody.velocity += Vector3.down * gravity * Time.deltaTime;
+        rigidbody.velocity += Vector3.down * m_gravity * Time.deltaTime;
     }
 }
